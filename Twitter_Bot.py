3d91d3ab0add2daa6d76850855da3238087edd81
Colin Joss <colindjoss@gmail.com>
2021-03-26 19:53:41 -0700

    authetication edits

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -1,13 +1,15 @@
 import tweepy
+import langdetect
+import secrets
+
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler()
-auth.set_access_token()
+auth = tweepy.OAuthHandler(secrets.CONSUMER_KEY, secrets.CONSUMER_SECRET)
+auth.set_access_token(secrets.ACCESS_KEY, secrets.ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
+if api.verify_credentials() is False:
     print("Error during authentication")
+
+
