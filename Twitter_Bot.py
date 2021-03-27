e95a6319cc962919a30cf4bf1e3070ef451497dc
Jacob Ogle <ogleja@oregonstate.edu>
2021-03-26 20:42:38 -0400

    Added starter code to store auth key and token data

---
+++ Twitter_Bot.py
@@ -0,0 +1,13 @@
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler()
+auth.set_access_token()
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
